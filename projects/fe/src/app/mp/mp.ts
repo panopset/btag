162b37b101985d568e35047635240efb4f47a7f2
Karl Dinwiddie <karl@panopset.com>
2026-03-01 15:17:36 -0800

    .

--- projects/fe/src/app/mp/mp.ts
+++ projects/fe/src/app/mp/mp.ts
@@ -14,20 +14,23 @@ export class Mp {
     name: new FormControl('')
   })
 
-  constructor(private dataService: DataService) {}
+  constructor(
+    private dataService: DataService
+  ) {}
 
   outputText = ''
 
   submitTheForm() {
     this.outputText = 'Loading data... '
     console.log('initiated submitTheForm...')
+    let nameField = this.userForm.get('name')
     let name = this.userForm.get('name')?.value
     if (name != null) {
      this.dataService.getData(name).subscribe({
        next: (resp) => {
          if (resp.body != null) {
-           this.outputText = resp.body
-           console.log('updated... ' + this.outputText)
+          this.outputText = resp.body
+          this.userForm.reset()
          }
        },
        error: (error) => {
