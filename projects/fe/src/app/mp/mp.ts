9c2bbf2bc9dfb966f7889240cad8bb8bb03e7468
Karl Dinwiddie <karl@panopset.com>
2026-03-01 19:03:52 -0800

    .

--- projects/fe/src/app/mp/mp.ts
+++ projects/fe/src/app/mp/mp.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
 import { FormsModule} from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { DataService } from '../services/data-service'
+import { map, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-mp',
@@ -11,13 +12,47 @@ import { DataService } from '../services/data-service'
 })
 export class Mp {
   constructor(
-    private dataService: DataService
+    private dataService: DataService,
+    private cdr: ChangeDetectorRef
   ) {}
 
   outputText = ''
   name = ''
 
   submitTheForm() {
+    this.outputText = 'Loading data... '
+    console.log('initiated submitTheForm...')
+    let submittedName = this.name
+    if (this.name != null) {
+     this.dataService.getData(this.name).pipe(
+       tap(_ => {
+         let reazy = _.body
+         console.log("reazy: " + reazy)
+         if (_.body != null) {
+           this.outputText = _.body
+         }
+       })
+     ).subscribe(
+       {
+              next: (resp) => {
+                if (resp.body != null) {
+                  this.cdr.detectChanges()
+         console.log("subscribe: " + resp.body)
+                 this.outputText = resp.body
+                 this.name = submittedName
+                }
+              },
+              error: (error) => {
+                console.log(error)
+              }
+            }
+       )
+    }
+  }
+
+
+
+  submitTheForm2() {
     this.outputText = 'Loading data... '
     console.log('initiated submitTheForm...')
     let submittedName = this.name
