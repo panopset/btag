4acfc16d8de1cf50bfd5c53b7a7fba7014eab485
Karl Dinwiddie <karl@panopset.com>
2026-03-01 16:36:52 -0800

    .

--- projects/fe/src/app/mp/mp.ts
+++ projects/fe/src/app/mp/mp.ts
@@ -1,36 +1,32 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators, ReactiveFormsModule} from '@angular/forms';
+import { FormsModule} from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { DataService } from '../services/data-service'
 
 @Component({
   selector: 'app-mp',
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './mp.html',
   styleUrl: './mp.scss',
 })
 export class Mp {
-  userForm = new FormGroup({
-    name: new FormControl('')
-  })
-
   constructor(
     private dataService: DataService
   ) {}
 
   outputText = ''
+  name = ''
 
   submitTheForm() {
     this.outputText = 'Loading data... '
     console.log('initiated submitTheForm...')
-    let nameField = this.userForm.get('name')
-    let name = this.userForm.get('name')?.value
-    if (name != null) {
-     this.dataService.getData(name).subscribe({
+    let submittedName = this.name
+    if (this.name != null) {
+     this.dataService.getData(this.name).subscribe({
        next: (resp) => {
          if (resp.body != null) {
           this.outputText = resp.body
-          this.userForm.reset()
+          this.name = submittedName
          }
        },
        error: (error) => {
